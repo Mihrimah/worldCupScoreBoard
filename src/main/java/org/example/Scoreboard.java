7308f1c25b317f61f7960c87ab3c7190b74208d8
Mihrimah Sultan Yildirim <mduydu@gmail.com>
2024-03-23 11:41:13 +0100

    the first test is introduced

---
+++ src/main/java/org/example/Scoreboard.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Scoreboard {
+    public void insertMatch(String homeTeam, String awayTeam) {
+
+    }
+
+    public boolean containsMatch(String homeTeam, String awayTeam) {
+        return true;
+    }
+}
---
+++ src/test/java/WorldCupScoreboardTest.java
@@ -0,0 +1,19 @@
+import org.example.Scoreboard;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WorldCupScoreboardTest {
+
+    @Test
+    public void test1() {
+        String homeTeam = "TeamA";
+        String awayTeam = "TeamB";
+
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.insertMatch(homeTeam, awayTeam);
+
+        assertTrue(scoreboard.containsMatch(homeTeam, awayTeam));
+    }
+}
+
